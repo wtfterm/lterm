24789bad2e4b397988babffe6dc64b0e5b23614e
wtfterm <150973972+wtfterm@users.noreply.github.com>
2024-04-06 14:54:52 -0400

    Update clickme.js

--- scripts/clickme.js
+++ scripts/clickme.js
@@ -1,9 +1,14 @@
 var fadey = document.getElementById('thingtofade');
 
 fadey.addEventListener('click', function() {
-    CoolThingy()
+    CoolThingy();
+    audioPlay();
 });
 function CoolThingy() {
   fadey.classList.toggle('fade');
   fadey.style.cssText = "pointer-events: none;"
+}
+function audioPlay() {
+  var audio = document.getElementById("video");
+  audio.play();
 }
