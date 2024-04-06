c1b13093c3d70dfb40e196f1054b8c39882c8419
wtfterm <150973972+wtfterm@users.noreply.github.com>
2024-04-06 14:53:02 -0400

    Update clickme.js

--- scripts/clickme.js
+++ scripts/clickme.js
@@ -1,5 +1,8 @@
 var fadey = document.getElementById('thingtofade');
 
+fadey.addEventListener('click', function() {
+    CoolThingy()
+});
 function CoolThingy() {
   fadey.classList.toggle('fade');
   fadey.style.cssText = "pointer-events: none;"
