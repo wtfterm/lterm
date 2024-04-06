562a9da0ebe4a4898a1c8a93b50ad98d7c6aecfa
wtfterm <150973972+wtfterm@users.noreply.github.com>
2024-04-06 19:20:17 -0400

    Update clipboard.js

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -1,4 +1,10 @@
-var 
-function clipboardcopy() {
-  navigator.clipboard.writeText("hey");
-}
+var ethereumi = document.getElementById('ethereum');
+var bitcoini = document.getElementById('bitcoin');
+
+ethereumi.addEventListener('click', function() {
+    navigator.clipboard.writeText("0xC48A97B2ec5342d348F19ed3cA0483eF7B1C0b87");
+});
+
+bitcoini.addEventListener('click', function() {
+    navigator.clipboard.writeText("bc1qqwacjmu34qvwhxdm4l7qq8sdep2dhasrseymhf");
+});
